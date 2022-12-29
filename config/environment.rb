833201829cb42c3fad2f03870db84b25067f6f36
Steven McVey <smcvey1210@gmail.com>
2022-12-29 14:48:17 -0800

    commit tree 0f134117d426b17b811d52bf6f6aa3c48e7ae688

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ ENV["RACK_ENV"] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
 require 'active_record'
+require 'pry'
 
 require_relative '../lib/dog'
 
@@ -10,3 +11,6 @@ ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: "./db/test.sqlite3"
 )
+
+binding.pry
+'hi'
